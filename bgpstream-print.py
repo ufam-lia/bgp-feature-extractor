ce879610c288ee67d92c1a6ca85d89d7a29e3d03
PC <pcdopc@gmail.com>
2018-03-22 19:37:43 -0400

    Tutorial de path inflation e detecção de MOAS

--- bgpstream-print.py
+++ bgpstream-print.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
+import time
 
 # Create a new bgpstream instance and a reusable bgprecord instance
 stream = BGPStream()
 rec = BGPRecord()
+l = []
+c = 0
 
 # Consider RIPE RRC 10 only
 stream.add_filter('collector','rrc11')
 
 # Consider this time interval:
 # Sat Aug  1 08:20:11 UTC 2015
-stream.add_interval_filter(1438417216,1438417216)
+stream.add_interval_filter(1438417216,1438418736)
 
 # Start the stream
 stream.start()
@@ -19,11 +22,29 @@ stream.start()
 while(stream.get_next_record(rec)):
 # Print the record information only if it is not a valid record
     if rec.status != "valid":
-      print rec.project, rec.collector, rec.type, rec.time, rec.status
+        print 'project: ' + str(rec.project)
+        print 'collector: ' + str(rec.collector)
+        print 'type: ' + str(rec.type)
+        print 'time: ' + str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(rec.time)))
+        print 'status: ' + str(rec.status)
+        print
     else:
-      elem = rec.get_next_elem()
-      while(elem):
-        # Print record and elem information
-        print rec.project, rec.collector, rec.type, rec.time, rec.status,
-        print elem.type, elem.peer_address, elem.peer_asn, elem.fields
         elem = rec.get_next_elem()
+        while(elem):
+            # Print record and elem information
+            # print 'project: ' + str(rec.project)
+            # print 'collector: ' + str(rec.collector)
+            # print 'type: ' + str(rec.type)
+            # print 'time: ' + str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(rec.time)))
+            # print 'status: ' + str(rec.status)
+            # print 'type: ' + str(elem.type)
+            # print 'peer_address: ' + str(elem.peer_address)
+            # print 'peer_asn: ' + str(elem.peer_asn)
+            # print 'fields: ' + str(elem.fields)
+            # print
+
+            l.append(str(elem.type))
+            c += 1
+            elem = rec.get_next_elem()
+print set(l)
+print c
---
+++ bgpstream-tutorial.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include "bgpstream.h"
+
+int
+main()
+{
+  /* Allocate memory for a bgpstream instance */
+  bgpstream_t *bs = bs = bgpstream_create();
+  if(!bs) {
+    fprintf(stderr, "ERROR: Could not create BGPStream instance\n");
+    return -1;
+  }
+
+  /* Allocate memory for a re-usable bgprecord instance */
+  bgpstream_record_t *bs_record = bgpstream_record_create();
+  if(bs_record == NULL)
+    {
+      fprintf(stderr, "ERROR: Could not create BGPStream record\n");
+      return -1;
+    }
+
+  /* The broker interface is set by default */
+
+  /* Select bgp data from RRC06 and route-views.jinx collectors only */
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "rrc06");
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_COLLECTOR, "route-views.jinx");
+
+  /* Process updates only */
+  bgpstream_add_filter(bs, BGPSTREAM_FILTER_TYPE_RECORD_TYPE, "updates");
+
+  /* Select a time interval to process:
+   * Sun, 10 Oct 2010 10:10:10 GMT -  Sun, 10 Oct 2010 11:11:11 GMT */
+  bgpstream_add_interval_filter(bs,1286705410,1286709071);
+
+  /* Start bgpstream */
+  if(bgpstream_start(bs) < 0) {
+    fprintf(stderr, "ERROR: Could not init BGPStream\n");
+    return -1;
+  }
+
+  int get_next_ret = 0;
+  int elem_counter = 0;
+
+  /* Pointer to a bgpstream elem, memory is borrowed from bgpstream,
+   * use the elem_copy function to own the memory */
+  bgpstream_elem_t *bs_elem = NULL;
+
+  /* Read the stream of records */
+  do
+    {
+      /* Get next record */
+      get_next_ret = bgpstream_get_next_record(bs, bs_record);
+      /* Check if there are new records and if they are valid records */
+      if(get_next_ret && bs_record->status == BGPSTREAM_RECORD_STATUS_VALID_RECORD)
+        {
+          /* Get next elem in the record */
+          while((bs_elem = bgpstream_record_get_next_elem (bs_record)) != NULL)
+            {
+              elem_counter++;
+            }
+        }
+    }
+  while(get_next_ret > 0);
+
+  /* Print the number of elems */
+  printf("Read %d elems\n", elem_counter);
+
+  /* De-allocate memory for the bgpstream record */
+  bgpstream_record_destroy(bs_record);
+
+  /* De-allocate memory for the bgpstream */
+  bgpstream_destroy(bs);
+
+  return 0;
+}
