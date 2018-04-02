0b493ac33793b28ee4ca2a01c9c1ad9f1fa2fb38
PC <pcdopc@gmail.com>
2018-04-02 02:30:36 -0400

    Plot time series

--- code-red-timeseries.py
+++ code-red-timeseries.py
@@ -10,7 +10,7 @@ from collections import defaultdict, OrderedDict
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
-
+import random
 import cPickle as pickle
 
 def dd():
@@ -36,17 +36,20 @@ def main():
     global count_prefixes_upds
     global timestamps
 
+    count_updates = 0
+
     #Traverse months
     # for i in [6,7,8,9,10,11]
     #Traverse files
-    files = glob.glob("/home/pc/ripe-ris/code-red/*.gz")
+    # files = glob.glob("/home/pc/ripe-ris/code-red/update*.gz")
+    files = glob.glob("/home/pc/ripe-ris/code-red/updates.20010701.*.gz")
     files = sorted(files)
 
-    c = 0
-
+    upds_per_file = []
     for f in files:
+        c = 0
+        count_updates = 0
         d = Reader(f)
-        c += 1
         current_day = dt.datetime.fromtimestamp(d.next().mrt.ts).date()
 
         for m in d:
@@ -56,19 +59,32 @@ def main():
                 continue
 
             c += 1
-            if c % 500 == 0:
-                print dt.datetime.fromtimestamp(m.ts)
+            # if c % 500 == 0:
+            #     print dt.datetime.fromtimestamp(m.ts)
 
             if m.type == MRT_T['BGP4MP'] or m.type == MRT_T['BGP4MP_ET']:
                 if m.subtype in BGPMessageST:
-                    if m.bgp.msg.type == BGP_MSG_T['UPDATE']:
-                        #Total updates per prefix
-                        if m.bgp.msg.nlri is not None:
-                            for nlri in m.bgp.msg.nlri:
-                                print str(nlri.prefix) + '/' + str(nlri.plen)
-                                # count_prefixes_ts[nlri.prefix + '/' + str(nlri.plen)][m.ts] += 1
-                                # count_prefixes_upds[nlri.prefix + '/' + str(nlri.plen)] += 1
-                                # timestamps[m.ts] = 1
+                    if m.bgp.msg is not None:
+                        if m.bgp.msg.type == BGP_MSG_T['UPDATE']:
+                            #Total updates per prefix
+                            count_updates += 1
+                            # if m.bgp.msg.nlri is not None:
+                            #     for nlri in m.bgp.msg.nlri:
+                                    # print str(nlri.prefix) + '/' + str(nlri.plen)
+                                    # count_prefixes_ts[nlri.prefix + '/' + str(nlri.plen)][m.ts] += 1
+                                    # count_prefixes_upds[nlri.prefix + '/' + str(nlri.plen)] += 1
+                                    # timestamps[m.ts] = 1
+        print f + ': ' + str(count_updates)
+
+        upds_per_file.append(count_updates)
+
+    aux = np.array(upds_per_file)
+
+    fig = plt.figure(1)
+    plt.subplot(1,1,1)
+    plt.plot(range(len(aux)), aux, lw=0.65, color = 'black')
+    fig.savefig(str(random.randint(1, 1000)),bboxes_inches = 'tight')
+    plt.show()
 
 if __name__ == '__main__':
     main()
