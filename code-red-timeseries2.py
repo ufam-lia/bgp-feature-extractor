9e393a4610cf69d668ef0054f579ba5f0621a9a2
Paulo César <pcdopc@gmail.com>
2018-04-04 18:49:47 -0400

    Added timeseries of all peer ASes

---
+++ code-red-timeseries2.py
@@ -0,0 +1,131 @@
+import sys
+import os, time
+import os.path
+from os.path import isfile
+from optparse import OptionParser
+import datetime as dt
+from mrtparse import *
+from collections import defaultdict, OrderedDict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+import random
+import cPickle as pickle
+import argparse
+
+def dd():
+    return defaultdict(int)
+
+count_prefixes_ts = defaultdict(dd)
+count_prefixes_upds = defaultdict(int)
+timestamps = OrderedDict()
+os.environ['TZ'] = 'US'
+time.tzset()
+
+BGPMessageST = [BGP4MP_ST['BGP4MP_MESSAGE'],BGP4MP_ST['BGP4MP_MESSAGE_AS4'],BGP4MP_ST['BGP4MP_MESSAGE_LOCAL'],BGP4MP_ST['BGP4MP_MESSAGE_AS4_LOCAL'],BGP4MP_ST['BGP4MP_MESSAGE_ADDPATH'],BGP4MP_ST['BGP4MP_MESSAGE_AS4_ADDPATH'],BGP4MP_ST['BGP4MP_MESSAGE_LOCAL_ADDPATH'],BGP4MP_ST['BGP4MP_MESSAGE_AS4_LOCAL_ADDPATH']]
+BGPStateChangeST = [ BGP4MP_ST['BGP4MP_STATE_CHANGE'], BGP4MP_ST['BGP4MP_STATE_CHANGE_AS4']]
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process BGP timeseries')
+    parser.add_argument('--days', type=int)
+    parser.add_argument('--start', type=str)
+    
+    args = parser.parse_args()
+    
+    #Traverse months
+    # for i in [6,7,8,9,10,11]
+    #Traverse files
+    # files = glob.glob("/home/pc/ripe-ris/code-red/update*.gz")
+    # files = glob.glob("/home/pc/ripe-ris/code-red/updates." + args.start + ".*.gz")
+    # files = glob.glob("/home/pc/ripe-ris/code-red/updates.20010710.*.gz")
+    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010711.*.gz")
+    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010712.*.gz")
+    files = glob.glob("/home/pc/ripe-ris/code-red/updates.20010713.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010714.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010715.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010716.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010717.*.gz")
+    files = sorted(files)
+
+    days = args.days
+    days_checked = 0
+    upds_per_file = []
+    announc_per_file = []
+    announc513_per_file = []
+    announc559_per_file = []
+    announc1755_per_file = []
+    announc6893_per_file = []
+    for f in files:
+        count_updates = 0
+        count_announcements = 0
+        count_updates_as513 = 0
+        count_updates_as559 = 0
+        count_updates_as1755 = 0
+        count_updates_as6893 = 0
+
+        d = Reader(f)
+        current_day = dt.datetime.fromtimestamp(d.next().mrt.ts).date()
+
+        for m in d:
+            m = m.mrt
+            if m.err == MRT_ERR_C['MRT Header Error']:
+                prerror(m)
+                continue
+
+            if current_day != dt.datetime.fromtimestamp(m.ts).date():
+                days_checked += 1
+                if days_checked <= days:
+                    break
+
+
+            if (m.type == MRT_T['BGP4MP'] or m.type == MRT_T['BGP4MP_ET']) \
+               and m.subtype in BGPMessageST \
+               and m.bgp.msg is not None \
+               and m.bgp.msg.type == BGP_MSG_T['UPDATE']:
+                #Total updates per prefix
+                count_updates += 1
+
+                if (m.bgp.msg.wd_len == 0) and (m.bgp.msg.attr_len > 0):
+                    count_announcements += 1
+
+                    if m.bgp.peer_as == '513':
+                        count_updates_as513 += 1
+                    if m.bgp.peer_as == '559':
+                        count_updates_as559 += 1
+                    if m.bgp.peer_as == '1755':
+                        count_updates_as1755 += 1
+                    if m.bgp.peer_as == '6893':
+                        count_updates_as6893 += 1
+
+        print f + ': ' + str(count_updates)
+
+        upds_per_file.append(count_updates)
+        announc_per_file.append(count_announcements)
+        announc513_per_file.append(count_updates_as513)
+        announc559_per_file.append(count_updates_as559)
+        announc1755_per_file.append(count_updates_as1755)
+        announc6893_per_file.append(count_updates_as6893)
+
+    timeseries = np.array(upds_per_file)
+    timeseries_ann = np.array(announc_per_file)
+    timeseries_ann_513 = np.array(announc513_per_file)
+    timeseries_ann_559 = np.array(announc559_per_file)
+    timeseries_ann_1755 = np.array(announc1755_per_file)
+    timeseries_ann_6893 = np.array(announc6893_per_file)
+
+    fig = plt.figure(1)
+    plt.subplot(1,1,1)
+    plt.plot(range(len(timeseries)), timeseries, lw=0.95, color = 'green')
+    plt.plot(range(len(timeseries_ann)), timeseries_ann, lw=0.95, color = 'orange', linestyle = '--')
+    plt.plot(range(len(timeseries_ann_513)), timeseries_ann_513, lw=0.95, color = 'red')
+    plt.plot(range(len(timeseries_ann_559)), timeseries_ann_559, lw=0.95, color = 'blue')
+    plt.plot(range(len(timeseries_ann_1755)), timeseries_ann_1755, lw=0.95, color = 'pink')
+    plt.plot(range(len(timeseries_ann_6893)), timeseries_ann_6893, lw=0.95, color = 'k')
+    fig.savefig(str(random.randint(1, 1000)), bboxes_inches = 'tight', dpi = 400)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+ 
