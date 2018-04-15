1df952504883aae6ba630bd8185fd84a5f996ff2
PC <pcdopc@gmail.com>
2018-04-15 14:30:57 -0400

    Added volume features

--- code-red-feature-extractor.py
+++ code-red-feature-extractor.py
@@ -14,6 +14,7 @@ import glob
 import random
 import cPickle as pickle
 import argparse
+import operator
 
 def dd():
     return defaultdict(int)
@@ -42,34 +43,37 @@ def main():
     files = []
     files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010713.*.gz")
     files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010714.*.gz")
-    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010715.*.gz")
-    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010716.*.gz")
-    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010717.*.gz")
-    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010718.*.gz")
-    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010719.*.gz")
-    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010720.*.gz")
-    # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010721.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010715.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010716.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010717.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010718.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010719.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010720.*.gz")
+    files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010721.*.gz")
     # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010722.*.gz")
     # files = files + glob.glob("/home/pc/ripe-ris/code-red/updates.20010723.*.gz")
     files = sorted(files)
 
     days = args.days
-    days_checked = 0
+    days_checked    = 0
     bin_size = 60
+    window_size = 60
     count_ts = defaultdict(int)
+    updates = defaultdict(int)
+    withdrawals = defaultdict(int)
+    announcements = defaultdict(int)
     count_origin = defaultdict(dd)
-    count_origin2 = defaultdict(dd)
     count_ts_upds_ases = defaultdict(dd)
-    count_ts_upds_prefixes = defaultdict(dd)
+    upds_prefixes = defaultdict(dd)
+    first_ts = 0
 
-    upds_per_file = []
-    announc_per_file = []
     for f in files:
         count_updates = 0
         count_announcements = 0
         d = Reader(f)
         current_day = dt.datetime.fromtimestamp(d.next().mrt.ts).date()
-        first_ts = d.next().mrt.ts
+        if first_ts == 0:
+            first_ts = d.next().mrt.ts
 
         for m in d:
             m = m.mrt
@@ -86,54 +90,69 @@ def main():
                and m.subtype in BGPMessageST \
                and m.bgp.msg is not None \
                and m.bgp.msg.type == BGP_MSG_T['UPDATE']:
-                #Total updates per prefix
-                bin = (m.ts - first_ts)/bin_size
                 count_updates += 1
-                count_ts[bin] += 1
-                count_ts_upds_ases[bin][m.bgp.peer_as] += 1
+                #Total number of annoucements/withdrawals/updates
+                bin = (m.ts - first_ts)/bin_size
+                updates[bin] += 1
+                # updates_ases[bin][m.bgp.peer_as] += 1
 
-                #Total updates per prefix
                 if m.bgp.msg.nlri is not None:
+                    announcements[bin] += 1
                     for nlri in m.bgp.msg.nlri:
-                        count_ts_upds_prefixes[bin][nlri.prefix + '/' + str(nlri.plen)] += 1
+                        upds_prefixes[bin][nlri.prefix + '/' + str(nlri.plen)] += 1
+
+                if (m.bgp.msg.wd_len > 0):
+                    withdrawals[bin] += 1
 
                 if m.bgp.msg.attr is not None:
                     for a in m.bgp.msg.attr:
                         if BGP_ATTR_T[a.type] == 'ORIGIN':
                             count_origin[bin][a.origin] += 1
-                            count_origin2[bin][ORIGIN_T[a.origin]] += 1
-
-                if (m.bgp.msg.wd_len == 0) and (m.bgp.msg.attr_len > 0):
-                    count_announcements += 1
 
         print f + ': ' + str(count_updates)
 
-    fig = plt.figure(1)
-    count_ts = OrderedDict(sorted(count_ts.items()))
+    #Ordering timeseries
+    updates = defaultdict(int, dict(sorted(updates.items(), key = operator.itemgetter(0))))
+    announcements = defaultdict(int, dict(sorted(announcements.items(), key = operator.itemgetter(0))))
+    withdrawals = defaultdict(int, dict(sorted(withdrawals.items(), key = operator.itemgetter(0))))
+
 
     #Filling blanks in the timeseries
-    for i in range(count_ts.keys()[-1]):
-        count_ts[i]
+    for i in range(updates.keys()[-1]):
+        updates[i]
 
+    for i in range(announcements.keys()[-1]):
+        announcements[i]
+
+    for i in range(withdrawals.keys()[-1]):
+        withdrawals[i]
+
+    fig = plt.figure(1)
     plt.subplot(1,1,1)
-    plt.plot(range(len(count_ts.keys())), count_ts.values(), lw=0.5, color = 'blue')
+    plt.plot(range(len(updates.keys())), updates.values(), lw=1.25, color = 'black')
+    plt.plot(range(len(announcements.keys())), announcements.values(), lw=0.5, color = 'blue')
+    plt.plot(range(len(withdrawals.keys())), withdrawals.values(), lw=0.5, color = 'red')
 
     # plt.xticks(range(0, len(count_ts.keys()), 86400), [datetime.fromtimestamp(x) for x in count_ts.keys() if (x % )])
     # plt.plot(range(len(timeseries_ann_6893)), timeseries_ann_6893, lw=0.95, color = 'k')
-    output = str(random.randint(1, 1000))
-    print output + '.png'
+    output = str(random.randint(1, 1000)) + '.png'
     fig.savefig(output, bboxes_inches = '30', dpi = 400)
+    print output
+    os.system('shotwell ' + output + ' &')
 
-    print 'first_ts -> ' + str(dt.datetime.fromtimestamp(first_ts).date())
-    print 'count_ts[bin] -> ' + str(count_ts)
-    print 'count_origin[bin] -> ' + str(count_origin2)
-    print 'count_ts_upds_prefixes[bin] -> ' + str(count_ts_upds_prefixes)
+    print 'first_ts -> ' + str(dt.datetime.fromtimestamp(first_ts))
+    # print 'count_origin[bin] -> ' + str(count_origin)
+    # print 'upds_prefixes[bin] -> ' + str(upds_prefixes)
 
-    for k, v in count_ts_upds_prefixes.iteritems():
+    for k, v in upds_prefixes.iteritems():
         print 'prefixes ' + str(k) + ' -> ' + str(len(v))
 
-    for k, v in count_ts.iteritems():
+    for k, v in updates.iteritems():
         print 'upds -> ' + str(k) + ' -> ' + str(v)
+
+    for k, v in upds_prefixes.iteritems():
+        print 'upds_prefixes -> ' + str(dt.datetime.fromtimestamp(first_ts + bin_size*k)) + ' -> ' + str(len(v))
+
     # plt.show()
 
 if __name__ == '__main__':
