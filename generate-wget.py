89db20df85248e9f2dee4ee0b07e52d9758f23d5
Paulo César <pcdopc@gmail.com>
2018-06-07 19:33:02 -0400

    Correction on wget generator

--- generate-wget.py
+++ generate-wget.py
@@ -3,13 +3,11 @@ import os
 
 rrc = sys.argv[1]
 path = 'ripe-ris/' + rrc
-os.system('mkdir ' + path)
-os.system('cd ' + path)
+os.system('mkdir -p ' + path)
 file = open(path + '/wget-ripe-' + rrc + '.sh','w')
 
-for col in xrange(5):
-    for year in xrange(2001,2004):
-        for month in xrange(1, 12):
-            file.write('wget -m -e robots=off --cut-dirs=3 --user-agent=Mozilla/5.0 --reject="index.html*" --no-parent --recursive --relative --level=1 --no-directories  http://data.ris.ripe.net/' + rrc + '/' + str(year) + '.' +str(month).zfill(2) + '/\n')
+for year in xrange(2001,2004):
+  for month in xrange(1, 13):
+    file.write('wget -m -e robots=off --cut-dirs=3 --user-agent=Mozilla/5.0 --reject="index.html*" --no-parent --recursive --relative --level=1 --no-directories  http://data.ris.ripe.net/' + rrc + '/' + str(year) + '.' +str(month).zfill(2) + '/\n')
 file.close()
-os.system('sudo chmod +x wget-ripe-' + rrc + '.sh')
+os.system('sudo chmod +x ' + path + '/wget-ripe-' + rrc + '.sh')
