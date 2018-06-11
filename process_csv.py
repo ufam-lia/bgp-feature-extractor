fe91cd381a222b23440dc030d9b3be3102e66f99
PC <pcdopc@gmail.com>
2018-06-11 10:40:52 -0400

    Ignore columns above threshold in order to dataframes have the same shape

--- process_csv.py
+++ process_csv.py
@@ -8,10 +8,18 @@ df = pd.DataFrame()
 
 for f in files:
     csv = pd.read_csv(f, index_col=0)
+    for i in xrange(11, 30):
+        col = 'edit_distance_dict_' + str(i) + '_'
+        if col in csv.keys():
+            csv.drop(col, 1, inplace = True)
+        col = 'edit_distance_unique_dict_' + str(i) + '_'
+        if col in csv.keys():
+            csv.drop(col, 1, inplace = True)
+    # print df.
     df = df.append(csv, sort = True)
 
 # df = df.drop(0, 1)
 # df = df.drop(1, 1)
 df.reset_index(drop = True, inplace = True)
-# df.drop(['timestamp', 'class'], 1, inplace = True)
+df.drop(['timestamp', 'class'], 1, inplace = True)
 print df
