3807295ac1a798347497dd82c9116fca90071f48
PC <pcdopc@gmail.com>
2018-06-12 16:03:40 -0400

    Separate train and test

--- process_csv.py
+++ process_csv.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import numpy as np
 import os, glob
 
 features_path = '/home/pc/bgp-feature-extractor/csv/'
 
-files = sorted(glob.glob(features_path + 'features-2001*'))
-df = pd.DataFrame()
+files_train = sorted(glob.glob(features_path + 'features-2001*'))
+files_test = sorted(glob.glob(features_path + 'features-nimda-2001*'))
 
-for f in files:
-    csv = pd.read_csv(f, index_col=0)
+df_train = pd.DataFrame()
+for f in files_train:
+    csv = pd.read_csv(f, index_col=0, delimiter = ';')
     for i in xrange(11, 30):
         col = 'edit_distance_dict_' + str(i) + '_'
         if col in csv.keys():
@@ -15,11 +17,29 @@ for f in files:
         col = 'edit_distance_unique_dict_' + str(i) + '_'
         if col in csv.keys():
             csv.drop(col, 1, inplace = True)
-    # print df.
-    df = df.append(csv, sort = True)
+    df_train = df_train.append(csv, sort = True)
 
-# df = df.drop(0, 1)
-# df = df.drop(1, 1)
-df.reset_index(drop = True, inplace = True)
-df.drop(['timestamp', 'class'], 1, inplace = True)
-print df
+df_test = pd.DataFrame()
+for f in files_test:
+    csv = pd.read_csv(f, index_col=0, delimiter = ';')
+    for i in xrange(11, 30):
+        col = 'edit_distance_dict_' + str(i) + '_'
+        if col in csv.keys():
+            csv.drop(col, 1, inplace = True)
+        col = 'edit_distance_unique_dict_' + str(i) + '_'
+        if col in csv.keys():
+            csv.drop(col, 1, inplace = True)
+    df_test = df_test.append(csv, sort = True)
+
+df_train.reset_index(drop = True, inplace = True)
+df_test.reset_index(drop = True, inplace = True)
+
+y_train = df_train['class']
+# y_test = df_test['class']
+
+df_train.drop(['timestamp', 'class'], 1, inplace = True)
+# df_test.drop(['timestamp', 'class'], 1, inplace = True)
+
+print df_train
+print '-'*220
+print df_test
