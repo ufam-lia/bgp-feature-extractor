b2f6d97584d0cb28c05d04da79ebec1b6077d58b
PC <pcdopc@gmail.com>
2018-07-04 09:26:12 -0400

    Replicated changes of mlp_detection.py Support to multiple LSTM layers

--- lstm_detection.py
+++ lstm_detection.py
@@ -1,30 +1,19 @@
+from __future__ import division
 import pandas as pd
 import numpy as np
-import os, glob
-
+import glob
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, LSTM
 from keras.optimizers import RMSprop
 import tensorflow as tf
+from keras import backend as K
 from keras.backend.tensorflow_backend import set_session
-import os
-from keras.layers import Dense
-from keras.layers import LSTM, Flatten
-from keras.layers.convolutional import Conv1D
-from keras.layers.convolutional import MaxPooling1D
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
-import warnings
+import os, sys
+from sklearn.preprocessing import MinMaxScaler
 
 from keras.callbacks import Callback
 from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Hide messy TensorFlow warnings
-warnings.filterwarnings("ignore") #Hide messy Numpy warnings
-
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-# os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 class Metrics(Callback):
     def on_train_begin(self, logs={}):
@@ -42,68 +31,174 @@ class Metrics(Callback):
         self.val_recalls.append(_val_recall)
         self.val_precisions.append(_val_precision)
         print ' - val_f1: %f - val_precision: %f - val_recall %f' %(_val_f1, _val_precision, _val_recall)
-
         return
+
+
+# Only compute a batch-wise average of recall.
+
+def recall(y_true, y_pred):
+    print    y_true
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1(y_true, y_pred):
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 metrics = Metrics()
 
-epochs = 20
+epochs = 10000
 batch_size = 32
-train_file = '/home/pc/bgp-feature-extractor/csv/train.csv'
-test_file = '/home/pc/bgp-feature-extractor/csv/test.csv'
+epsilon = 0.000000000000001
 
-x_train = pd.read_csv(train_file, index_col = 0)
+rrc = sys.argv[1]
+train_file = '/home/pc/bgp-feature-extractor/datasets/top10_train_' + rrc + '.csv'
+test_file = '/home/pc/bgp-feature-extractor/datasets/top10_train_' + rrc + '.csv'
+
+
+x_train = pd.read_csv(train_file, index_col = 0, delimiter=',')
 y_train = x_train['class']
-x_train.drop(['class','timestamp'], 1, inplace = True)
+x_train.drop(['class', 'timestamp', 'timestamp2'], 1, inplace = True)
 
-x_test = pd.read_csv(test_file, index_col = 0)
+x_test = pd.read_csv(test_file, index_col = 0,delimiter=',')
 y_test = x_test['class']
-x_test.drop(['class','timestamp','timestamp2'], 1, inplace = True)
+x_test.drop(['class', 'timestamp', 'timestamp2'], 1, inplace = True)
 
+print x_train.keys()
+print x_test.keys()
 x_train = x_train.values
 y_train = y_train.values
 x_test = x_test.values
 y_test = y_test.values
+
 print x_train.shape
-# x_train = x_train.reshape(-1, 44)
-# x_test = x_test.reshape(-1, 44)
+print x_test.shape
+
 x_train = x_train.reshape(x_train.shape[0], 1, x_train.shape[1])
 x_test = x_test.reshape(x_test.shape[0], 1, x_test.shape[1])
-print x_train.shape
-# y_train = y_train.reshape(-1, 1)
-# y_test = y_test.reshape(-1, 1)
+
+# x_train = x_train.reshape(-1, 9)
+# x_test = x_test.reshape(-1, 9)
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-# y_train = x_train.astype('float32')
-# y_test = x_test.astype('float32')
+y_train = y_train.astype('float32')
+y_test = y_test.astype('float32')
+
+x_train -= x_train.mean(axis = 0)
+x_train /= x_train.std(axis = 0)
+x_test -= x_test.mean(axis = 0)
+x_test /= x_test.std(axis = 0)
+
+# np.savetxt('oi.csv', x_train, delimiter=';')
+print 'oi.csv'
+# print '*'*100
+# print x_train
+# print np.round(x_train.mean(axis = 0))
+# print np.round(x_train.std(axis = 0))
+# scaler = MinMaxScaler(feature_range=(0, 1))
+# x_train = scaler.fit_transform(x_train)
+# print x_train
+# x_test = scaler.fit_transform(x_test)
+# print x_test
 
 model = Sequential()
-# print x_train[0].shape
-# model.add(Dense(10, activation='relu', input_shape=(44, )))
-# model.add(Dense(10, activation='tanh'))
-# model.add(Dense(10, activation='relu'))
-# model.add(Conv1D(filters = 32, kernel_size = 3, padding = 'same', activation = 'relu'))
-# model.add(MaxPooling1D(pool_size = 2))
-# model.add(LSTM(10, return_sequences = False))
-model.add(LSTM(10, return_sequences = False, input_shape = (1, 44)  ))
-# model.add(Dense(10, activation='relu'))
-# model.add(Flatten())
+model.add(Dense(256, activation='relu', input_shape = (x_train[0].shape)))
+# # model.add(Dropout(0.2))
+# model.add(Dense(256, activation='relu'))
+# # model.add(Dropout(0.2))
+model.add(LSTM(20, return_sequences = True))
+model.add(LSTM(20, return_sequences = True))
+model.add(LSTM(20, return_sequences = False))
+# model.add(LSTM(100, return_sequences = False))
 model.add(Dense(1, activation='sigmoid'))
 
 model.summary()
-
 model.compile(loss='binary_crossentropy',
-              optimizer=RMSprop(lr = 0.0001),
-              # optimizer=RMSprop(),
+              optimizer=RMSprop(lr=0.00001),
               metrics=['accuracy'])
 
-history = model.fit(x_test, y_test,
+validation_data = x_test
+validation_target = y_test
+class_weight = {0: 1.,  1: 6.}
+
+history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
-                    validation_data=(x_train, y_train))
+                    # validation_data=(validation_data, validation_target),
+                    class_weight=class_weight)
                     # callbacks=[metrics])
-score = model.evaluate(x_train, y_train, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+
+y_pred = model.predict(x_test, verbose = 2).round()
+tp = 0
+tn = 0
+fp = 0
+fn = 0
+pos = 0
+neg = 0
+pred_pos = 0
+pred_neg = 0
+
+for i in xrange(len(y_pred)):
+    print str(y_pred[i]) + ' == ' + str(y_test[i])
+    if y_pred[i] == y_test[i]:
+        if y_test[i] == 1:
+            tp += 1
+            pos += 1
+            pred_pos += 1
+        else:
+            tn += 1
+            neg += 1
+            pred_neg += 1
+    else:
+        if y_test[i] == 1:
+            fn += 1
+            pos += 1
+            pred_neg += 1
+        else:
+            fp += 1
+            neg += 1
+            pred_pos += 1
+
+print '--------------'
+print 'pos->' + str(pos)
+print 'neg->' + str(neg)
+print 'pred_pos->' + str(pred_pos)
+print 'pred_neg->' + str(pred_neg)
+print '--------------'
+print 'tp->' + str(tp)
+print 'tn->' + str(tn)
+print 'fp->' + str(fp)
+print 'fn->' + str(fn)
+print '--------------'
+
+# print type(y_test[0][0])
+# print type(y_pred[0][0])
+
+acc = (tp + tn)/(tp + tn + fp + fn)
+print 'acc->' + str(np.round(acc*100, decimals=2)) + '%'
+precision = tp/(tp + fp + epsilon)
+print 'precision->' + str(np.round(precision*100, decimals=2)) + '%'
+# print 'precision->' + str(precision_score(y_test, y_pred.round())*100) + '%'
+recall = (tp)/(tp + fn)
+print 'recall->' + str(np.round(recall*100, decimals=2)) + '%'
+# print 'recall->' + str(np.round(recall_score(y_test, y_pred.round())*100, decimals=2)) + '%'
+f1 = 2*(precision*recall)/(precision + recall + epsilon)
+print 'f1->' + str(np.round(f1*100, decimals=2)) + '%'
+# print 'f1->' + str(np.round(f1_score(y_test, y_pred.round())*100, decimals=2)) + '%'
+
+# score = model.evaluate(x_test, y_test, verbose = 2)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
