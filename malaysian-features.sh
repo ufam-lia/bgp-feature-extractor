587a8331f613197e5bffa3c2a7892852b39503e2
PC <pcdopc@gmail.com>
2018-08-16 08:46:38 -0400

    Anomalies scripts

--- malaysian-features.sh
+++ malaysian-features.sh
@@ -1,4 +1,4 @@
-# time python feature-extractor.py rrc04 513 malaysian-telecom 5 &
-time python feature-extractor.py rrc04 34781 malaysian-telecom 5 &
-time python feature-extractor.py rrc04 25091 malaysian-telecom 5 &
-time python feature-extractor.py rrc04 20932 malaysian-telecom 5 &
+time python feature-extractor.py rrc04 513 malaysian-telecom 10 
+time python feature-extractor.py rrc04 34781 malaysian-telecom 10 
+time python feature-extractor.py rrc04 25091 malaysian-telecom 10 
+time python feature-extractor.py rrc04 20932 malaysian-telecom 10 
