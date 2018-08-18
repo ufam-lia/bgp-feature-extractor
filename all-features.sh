b99c8e3a4f026581b236096231fbca35f3d603f1
PC <pcdopc@gmail.com>
2018-08-18 00:10:00 -0400

    All anomalies with timescale of 1-min

--- all-features.sh
+++ all-features.sh
@@ -1,3 +1,4 @@
+: '
 time python feature-extractor.py rrc03 3257 as-3561-filtering 1 &
 time python feature-extractor.py rrc03 3333 as-3561-filtering 1 &
 time python feature-extractor.py rrc03 286 as-3561-filtering 1
@@ -6,19 +7,21 @@ time python feature-extractor.py rrc05 12793 as9121 1 &
 time python feature-extractor.py rrc05 13237 as9121 1 &
 time python feature-extractor.py rrc05 1853 as9121 1
 
-time python feature-extractor.py rrc01 13237 as-depeering 1 &
+time python feature-extractor.py rrc01 13237 as-depeering 1 
 time python feature-extractor.py rrc01 8342 as-depeering 1
-time python feature-extractor.py rrc01 5511 as-depeering 1 &
+time python feature-extractor.py rrc01 5511 as-depeering 1 
 time python feature-extractor.py rrc01 16034 as-depeering 1
+'
 
 time python feature-extractor.py rrc03 3257 as-path-error 1
 time python feature-extractor.py rrc03 3333 as-path-error 1
 time python feature-extractor.py rrc03 286 as-path-error 1
 time python feature-extractor.py rrc03 9057 as-path-error 1
 
-# time python feature-extractor.py rrc04 15547 aws-leak 1
-# time python feature-extractor.py rrc04 25091 aws-leak 1
-# time python feature-extractor.py rrc04 34781 aws-leak 1
+:'
+time python feature-extractor.py rrc04 15547 aws-leak 1
+time python feature-extractor.py rrc04 25091 aws-leak 1
+time python feature-extractor.py rrc04 34781 aws-leak 1
 
 time python feature-extractor.py rrc04 513 code-red 1 &
 time python feature-extractor.py rrc04 559 code-red 1 &
@@ -33,10 +36,11 @@ time python feature-extractor.py rrc05 1853 moscow-blackout 1 &
 time python feature-extractor.py rrc05 12793 moscow-blackout 1 &
 time python feature-extractor.py rrc05 13237 moscow-blackout 1 &
 
-# time python feature-extractor.py rrc04 513 nimda 10 &
-# time python feature-extractor.py rrc04 559 nimda 10 &
-# time python feature-extractor.py rrc04 6893 nimda 10 &
-#
-# time python feature-extractor.py rrc04 513 slammer 10 &
-# time python feature-extractor.py rrc04 559 slammer 10 &
-# time python feature-extractor.py rrc04 6893 slammer 10 &
+time python feature-extractor.py rrc04 513 nimda 10 &
+time python feature-extractor.py rrc04 559 nimda 10 &
+time python feature-extractor.py rrc04 6893 nimda 10 &
+
+time python feature-extractor.py rrc04 513 slammer 10 &
+time python feature-extractor.py rrc04 559 slammer 10 &
+time python feature-extractor.py rrc04 6893 slammer 10 &
+'
