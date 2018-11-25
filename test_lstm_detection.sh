f6124aa7cebab9bf8a79308062eb87f895060338
PC <pcdopc@gmail.com>
2018-11-25 17:07:06 -0400

    Predictions

--- test_lstm_detection.sh
+++ test_lstm_detection.sh
@@ -4,6 +4,6 @@ time python lstm_detection.py --epochs 100 --lag 20 --test code-red
 time python lstm_detection.py --epochs 100 --lag 20 --test nimda
 time python lstm_detection.py --epochs 100 --lag 20 --test as9121
 time python lstm_detection.py --epochs 100 --lag 20 --test aws-leak
-time python lstm_detection.py --epochs 100 --lag 20 --test malaysian_telecom
+time python lstm_detection.py --epochs 100 --lag 20 --test malaysian-telecom
 time python lstm_detection.py --epochs 100 --lag 20 --test japan-earthquake
 time python lstm_detection.py --epochs 100 --lag 20 --test moscow-blackout
